07bb41cc9b2c121c66c3aeca5a899099f623428d
Vitor Fernandes <vitor.fernandes@blip.pt>
2016-09-19 00:08:07 +0100

    Added download link functionality

--- popup.js
+++ popup.js
@@ -1,14 +1,27 @@
 //Event that run right after page DOM loaded
 document.addEventListener('DOMContentLoaded', function() {
 
+  var tClient = null;
+
+  syncLocalData(function(url,user,pass,token){
+    tClient = new TransmissionClient(url,user,pass,pass,token);
+  });
+
+  var addTorrent=function(url){
+    tClient.addTorrent(url,
+      function(){alert("Added torrent: "+url);},
+      function(){alert("Error while sending torrent");}
+    )
+  };
+
   var renderResults = function(result,number){
     var renderHtml = "";
 
     result.message.slice(0,number).forEach(function(tinfo){
       renderHtml += "<div class='torrentInfo'>";
       renderHtml += "<div class='name'>"+tinfo.name+"</div>";
-      renderHtml += "<p><a href='"+tinfo.magnetLink+"'>MagnetLink</a></p>";
-      renderHtml += "<p><a href='"+tinfo.torrentLink+"'>TorrentLink</a></p>";
+      renderHtml += "<p><a href='#' data='"+btoa(tinfo.magnetLink)+"'>MagnetLink</a></p>";
+      renderHtml += "<p><a href='#' data='"+btoa(tinfo.magnetLink)+"'>TorrentLink</a></p>";
       renderHtml += "<p><span>"+tinfo.date+"</span></p>";
       renderHtml += "<p><span>Seeders: "+tinfo.seeders+"</span></p>";
       renderHtml += "<p><span>Leechers: "+tinfo.leechers+"</span></p>";
@@ -16,10 +29,18 @@ document.addEventListener('DOMContentLoaded', function() {
     });
 
     divResults.innerHTML = renderHtml;
+
+    var aEl = divResults.getElementsByTagName('a');
+    Array.from(aEl).forEach(function(el){
+      var data = el.getAttribute("data");
+      el.onclick = function(){
+        addTorrent(atob(data));
+      }
+    });
   };
 
-  var render10 = function(result){
-    renderResults(result,10);
+  var render100 = function(result){
+    renderResults(result,100);
   }
 
   var renderError = function(xhr){
@@ -32,6 +53,6 @@ document.addEventListener('DOMContentLoaded', function() {
 
   btnSearch.addEventListener('click',function(){
     var pb=new WebPT.PirateBay.API(service_url);
-    pb.searchTorrents(txtSearch.value,0,render10,renderError);
+    pb.searchTorrents(txtSearch.value,0,render100,renderError);
   });
 });
