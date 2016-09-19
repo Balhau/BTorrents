35eed0962050ead79fc1159ca1af1ffbe9b706d8
Vitor Fernandes <vitor.fernandes@blip.pt>
2016-09-19 01:03:01 +0100

    Removing some more bugs. A little bit of gardening

--- yts-injector.js
+++ yts-injector.js
@@ -1,30 +1,29 @@
 $(document).ready(function() {
   syncLocalData(function(url,user,pass,token){
-
     tClient = new TransmissionClient(url,user,pass,pass,token);
-    tClient.sessionToken = sessionToken
-
     console.log(tClient);
-
-    var links=document.getElementById('movie-info').getElementsByTagName('p')[0].getElementsByTagName('a');
-    Array.from(links).forEach(
-      function(link){
-        link.onclick=function(){
-          var l=this.href;
-          this.href="#";
-          var validLink = l.split(".torrent").length != 1;
-          console.log(l.split(".torrent"));
-          if(validLink){
-            tClient.addTorrent(l,
-            function(){
-                alert("Torrent added successfully");
-            },
-            function(){
-              alert("Error while downloading torrent");
-            }
-          )
-        }
-      };
-    });
   });
+
+  var links=document.getElementById('movie-info').getElementsByTagName('p')[0].getElementsByTagName('a');
+
+  Array.from(links).forEach(
+    function(link){
+      link.onclick=function(){
+        var l=this.href;
+        var validLink = l.split(".torrent").length != 1;
+        console.log(l.split(".torrent"));
+        if(validLink){
+          tClient.addTorrent(l,
+          function(){
+              alert("Torrent added successfully");
+          },
+          function(){
+            alert("Error while downloading torrent");
+          }
+        )
+      }
+      return false;
+    };
+  });
+
 });
